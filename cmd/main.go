def32146bed5f108102519bbf75ce1d86ca199bc
xRosemary <2862656629@qq.com>
2023-04-15 20:27:55 +0800

    chore: 修改端口号，添加注释

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,11 @@ import (
 func main() {
 	server := socks5.SOCKS5Server{
 		IP:   "localhost",
-		Port: 1080,
+		Port: 7893,
 		Config: &socks5.Config{
 			AuthMethod: socks5.MethodPassword,
 			PasswordChecker: func(username, password string) bool {
+				// TODO 完善账号验证机制
 				return true
 			},
 			TCPTimeout: 5 * time.Second,
